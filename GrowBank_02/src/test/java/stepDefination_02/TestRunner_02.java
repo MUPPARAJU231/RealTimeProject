6a76a97e005d22724bda2cbe6a1d772d8e5f485b
Mupparaju231 <veeranji231@gmail.com>
2025-05-25 17:04:03 +0530

    update Report

--- GrowBank_02/src/test/java/stepDefination_02/TestRunner_02.java
+++ GrowBank_02/src/test/java/stepDefination_02/TestRunner_02.java
@@ -12,11 +12,7 @@ import io.cucumber.junit.CucumberOptions;
 		dryRun=false,
 		monochrome=false,
 		plugin= {"pretty","html:target/HtmlReport/cucumberReport.html",
-<<<<<<< HEAD
-				  "json:target/cucumber.json",
-=======
-				  "json:target/Report.json",
->>>>>>> c234c72f264793363f85f02e745269598bfa9d0b
+  				          "json:target/Report.json",
 			 
 			"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
 		}
